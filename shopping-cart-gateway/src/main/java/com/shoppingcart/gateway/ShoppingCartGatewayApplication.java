88db1066feb9aec7527bb9ef5b1b854212c395b5
chetanpatel2510 <chetanpatel2510@gmail.com>
2019-05-27 23:52:54 +0530

    Gateway Initial Commit

---
+++ shopping-cart-gateway/src/main/java/com/shoppingcart/gateway/ShoppingCartGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.shoppingcart.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ShoppingCartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartGatewayApplication.class, args);
+	}
+
+}
