9db8b508f5987010923c50a993a4b27d9dcaa204
chetanpatel2510 <chetanpatel2510@gmail.com>
2019-06-08 01:28:48 +0530

    corrected the imports

--- shopping-cart-gateway/src/main/java/com/shoppingcart/gateway/filters/JwtAuthorizationFilter.java
+++ shopping-cart-gateway/src/main/java/com/shoppingcart/gateway/filters/JwtAuthorizationFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
