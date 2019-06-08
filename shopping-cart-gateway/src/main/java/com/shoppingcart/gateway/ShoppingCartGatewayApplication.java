d23fef16e10709c21cdd525ac3a3410c44db0d67
chetanpatel2510 <chetanpatel2510@gmail.com>
2019-06-08 22:19:07 +0530

    Removed spring session implementation.

--- shopping-cart-gateway/src/main/java/com/shoppingcart/gateway/ShoppingCartGatewayApplication.java
+++ shopping-cart-gateway/src/main/java/com/shoppingcart/gateway/ShoppingCartGatewayApplication.java
@@ -3,13 +3,11 @@ package com.shoppingcart.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableZuulProxy
 @SpringBootApplication
 @CrossOrigin
-@EnableRedisHttpSession
 public class ShoppingCartGatewayApplication {
 
 	public static void main(String[] args) {
